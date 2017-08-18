76446c8876dc2f32ad8aa88f6fb92104c0ae5fa9
deano1949 <gly1949@gmail.com>
2017-08-19 00:17:21 +0100

    update trading simulation

--- Testing_Master_sys_trading_simulation.m
+++ Testing_Master_sys_trading_simulation.m
@@ -1,5 +1,6 @@
 
 %% Description: Trade simulation for THE SYSTEM
+%This test file to test the "Master_sys_trading_simulation" script on VIX carry signal. It ensures the end-to-end trading simulation is 100% accurate
 
 %% load data
 load SYS_beta.mat
@@ -23,14 +24,10 @@ end
 %% Setup
 AUM=1000000;
 vol_target=0.2;
-listF={'SPX','VIX','WTI','USZC'};
+listF={'VIX'};
 listSubsysdat=struct;
-listSubsysdat.(listF{1})=EquityData.SPX;
-listSubsysdat.(listF{2})=EquityData.VIX;
-listSubsysdat.(listF{3})=ComdtyData.WTI;
-listSubsysdat.(listF{4})=Bond10YData.USZC;
-
-contract_size=[50 1000 1000 50]; %dummy to be automate
+listSubsysdat.(listF{1})=EquityData.VIX;
+contract_size=[1000]; %dummy to be automate
 
 timestamp=setting.timestamp;
 timenum=datenum(timestamp,'dd/mm/yyyy');
@@ -41,23 +38,23 @@ signalmat=NaN(size(timenum,1),size(listF,2));
 volmat=NaN(size(timenum,1),size(listF,2));
 %subsystem
 namefld=fieldnames(FamilySubsys);
-for i=1:length(listF)
+for i=1
     subsysdat=listSubsysdat.(listF{i});
-    subsys=FamilySubsys.(namefld{i});
+    subsys=FamilySubsys.(namefld{2});
     xts=tsvlookup(timenum,datenum(subsysdat.timestamp,'dd/mm/yyyy'),subsysdat.Generic123Price.(2)); %vlookup on timenum
     xmat(:,i)=xts(:,2);
     xretts=tsvlookup(timenum,datenum(subsysdat.timestamp,'dd/mm/yyyy'),subsysdat.Generic12Return.(2)); %vlookup on timenum
     xretmat(:,i)=xretts(:,2);
-    signalts=tsvlookup(timenum,datenum(subsys.timestamp,'dd/mm/yyyy'),subsys.Signal);
+    signalts=tsvlookup(timenum,datenum(subsys.timestamp,'dd/mm/yyyy'),subsys.StratSignal.Carry);
     signalmat(:,i)=signalts(:,2);
     volts=tsvlookup(timenum,datenum(subsysdat.timestamp,'dd/mm/yyyy'),smartMovingStd(subsysdat.Generic12Return.(2),25));
     volmat(:,i)=volts(:,2);
 end
 
-fx=repmat([1 1 1 1],size(timenum,1),1);
-weight=repmat(transpose(sys.wgts),size(timenum,1),1); %instrument weights
+fx=ones(size(timenum,1),1); %to be automate
+weight=ones(size(timenum,1),1); %instrument weights
 diversifer=1;
-bidask_spread=[0.0001 0.0003 0.0003 0.0003];%dummy to be automate
+bidask_spread=0.0003;%dummy to be automate
 % bidask_spread=[0.000 0.000 0.000 0.000];%dummy to be automate
 
 matt= TradeSimT2(AUM,vol_target,contract_size,xmat,xretmat,signalmat,...
